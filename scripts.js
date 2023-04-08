c135538043c8154cc5b4d3b5bf71d8832dba2b37
Jordan <jdegrez1@gmail.com>
2023-04-08 18:15:11 +0200

    hop

--- scripts.js
+++ scripts.js
@@ -8,19 +8,18 @@ for (let i = 0; i < $hoverables.length; i++) {
   $hoverables[i].addEventListener('mouseenter', onMouseHover);
   $hoverables[i].addEventListener('mouseleave', onMouseHoverOut);
 }
-
 // Move the cursor
 function onMouseMove(e) {
-  TweenMax.to($bigBall, .4, {
-    x: e.pageX - 15,
-    y: e.pageY - 15
-  })
-  TweenMax.to($smallBall, .1, {
-    x: e.pageX - 5,
-    y: e.pageY - 7
-  })
+    TweenMax.to($bigBall, .4, {
+      x: e.pageX - 15,
+      y: e.pageY - 15
+    })
+    TweenMax.to($smallBall, .1, {
+      x: e.pageX - 5,
+      y: e.pageY - 7
+    })
 }
-
+  
 // Hover an element
 function onMouseHover() {
   TweenMax.to($bigBall, .3, {
